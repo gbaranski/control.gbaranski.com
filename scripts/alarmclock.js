9f7b89c90eb6cecd67394b0bb22c200c2c5977c3
gbaranski <gbaranski19@gmail.com>
2020-04-19 13:52:55 +0200

    favicon, split in two js files, window focus check

--- scripts/alarmclock.js
+++ scripts/alarmclock.js
@@ -36,39 +36,6 @@ function setAlarmTime() {
   xhttp.send();
 }
 
-function setWaterMixerDataInPlace(jsonObject) {
-  console.log(jsonObject)
-  document.getElementById("waterMixerState").innerText = jsonObject.isTimerOn == "1" ? "ON" : "OFF";
-  document.getElementById("waterMixerMinutes").innerText = Math.floor((jsonObject.remainingSeconds / 60) % 60) + "minutes";
-  document.getElementById("waterMixerSeconds").innerText = jsonObject.remainingSeconds % 60 + "seconds";
-}
-
-function getWaterMixerDataFromRemote() {
-
-  let xhttp = new XMLHttpRequest;
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      setWaterMixerDataInPlace(JSON.parse(this.responseText));
-    }
-  };
-  xhttp.open("GET", "https://control.gbaranski.com/getWaterMixerESPData", true)
-  xhttp.send();
-}
-
-function startMixingWater() {
-  let xhttp = new XMLHttpRequest;
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log("GOOD RESPONSE");
-      getWaterMixerDataFromRemote();
-    } else {
-      console.log("Something went wrong");
-    }
-  };
-  xhttp.open("GET", "https://control.gbaranski.com/startMixing", true)
-  xhttp.send();
-}
-
 
 $(function () {
   getAlarmDataFromRemote();
@@ -82,7 +49,3 @@ $(function () {
     startMixingWater();
   });
 });
-
-window.onload = function () {
-  console.log("123321312");
-}
