8dfd6130c2b8c2f07e285ccaaed35126746ee0c0
gbaranski <gbaranski19@gmail.com>
2020-05-15 22:59:09 +0200

    add newest build, remove local bootstrap

---
+++ build/precache-manifest.0f9c0e9d7c10c5355b49239f5aa67e2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d9d19df564dedf3d23ab2c18f384c9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7220c0b5eb0697359517",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4edabd520f5d35795ab4",
+    "url": "/static/css/main.5a4cfae2.chunk.css"
+  },
+  {
+    "revision": "7220c0b5eb0697359517",
+    "url": "/static/js/2.6cddaef1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6cddaef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4edabd520f5d35795ab4",
+    "url": "/static/js/main.5ba6f944.chunk.js"
+  },
+  {
+    "revision": "b247cda3aada520648ed",
+    "url": "/static/js/runtime-main.78a5f9f4.js"
+  },
+  {
+    "revision": "7dd418c2e650c6aed3f682899eab8514",
+    "url": "/static/media/humidity.7dd418c2.png"
+  }
+]);
