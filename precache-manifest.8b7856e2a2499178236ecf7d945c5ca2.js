08de5b54decdf745fa2012f23b12730e5ea7454f
gbaranski <gbaranski19@gmail.com>
2020-05-27 18:19:54 +0200

    Updates

---
+++ precache-manifest.8b7856e2a2499178236ecf7d945c5ca2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c2a898f2d77ad7a497b769ffbfedf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e8aa5185d46f700b6f7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1ec8e1338e7a975331f7",
+    "url": "/static/css/main.5a4cfae2.chunk.css"
+  },
+  {
+    "revision": "2e8aa5185d46f700b6f7",
+    "url": "/static/js/2.da5db52c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.da5db52c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec8e1338e7a975331f7",
+    "url": "/static/js/main.b7738f6d.chunk.js"
+  },
+  {
+    "revision": "b247cda3aada520648ed",
+    "url": "/static/js/runtime-main.78a5f9f4.js"
+  },
+  {
+    "revision": "7dd418c2e650c6aed3f682899eab8514",
+    "url": "/static/media/humidity.7dd418c2.png"
+  }
+]);
