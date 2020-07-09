8c3e0d958eae83582aba433194afe17ff88cfdbb
gbaranski <gbaranski19@gmail.com>
2020-07-09 20:15:47 +0200

    removed unused imports

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,7 +8,6 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import {makeStyles} from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
