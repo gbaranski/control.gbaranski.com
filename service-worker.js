a47de0bb2abd20b2eec2cac4f05f9bd9a19dfa5c
gbaranski <gbaranski19@gmail.com>
2020-07-09 03:43:07 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02fc9a2e14cc1759439302369be93a54.js"
+  "/precache-manifest.52d812905a2fdf56de34d2a302cafa9c.js"
 );
 
 self.addEventListener('message', (event) => {
